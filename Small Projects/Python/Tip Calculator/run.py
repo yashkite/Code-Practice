5aa417373512f1b7f0ca5becae9587e3df9a023d
Yash Kite <yashkite21@gmail.com>
2023-10-05 18:38:31 +0530

    result shows only 2 digit after decimal
    added functionality to shows only 2 digit after decimal

--- Small Projects/Python/Tip Calculator/run.py
+++ Small Projects/Python/Tip Calculator/run.py
@@ -2,7 +2,7 @@ total_bill = input("What is the Total Bill?: ")
 tip_percentage = input("Enter the Tip Percentage: ")
 split_number = input("In how many peoples have to split the bill: ")
 
-per_person = str(((float(tip_percentage)/100 * float(total_bill)) + float(total_bill) )/float(split_number))
+per_person = "{:.2f}".format(((float(tip_percentage)/100 * float(total_bill)) + float(total_bill) )/float(split_number))
 
 
-print("Per person will have to pay: $" + per_person )
+print("Per person will have to pay: $" + per_person)
