54193b45517f342debd6e622f761b274cbb3668d
Yash Kite <yashkite21@gmail.com>
2023-10-12 21:07:03 +0530

    base project for FizzBuzz
    base project for FizzBuzz

---
+++ Small Projects/Python/FizzBuzz/run.py
@@ -0,0 +1,9 @@
+for x in range(1,101):
+    if x%3 is 0 and x%5 is 0:
+        print("FizzBuzz")
+    elif x%3 is 0:
+        print("Fizz")
+    elif x%5 is 0:
+        print("Buzz")
+    else:
+        print(x)
