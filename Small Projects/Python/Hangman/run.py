abe064f2f249138908d1bf6e3c70eff2e1cb0058
Yash Kite <yashkite21@gmail.com>
2023-10-17 21:31:36 +0530

    update hangman project
    with this now we can illustrate how many life is remaining while playing the game

--- Small Projects/Python/Hangman/run.py
+++ Small Projects/Python/Hangman/run.py
@@ -24,31 +24,40 @@ for x in range(0,len(fruit)):
 
 game_state = True
 
+chance = 0
+
 while game_state:
 
     guess = input("Guess the letter in the word: ")
     
-    # is_word_present = True
+    is_word_present = False
 
-    for letter in range(0,len(fruit)):
-
-        if fruit[letter] == guess:
-            out[letter]= guess         
+    for letter in fruit:
+        if letter is guess:
+            is_word_present = True
     
+    if is_word_present and chance != 7:
+        for letter in range(0,len(fruit)):
+            if fruit[letter] == guess:
+                out[letter]= guess         
+        
+        check_letters = len(fruit) 
 
-    
+        for letter in range(0,len(fruit)):
+            
+            if out[letter] != "_":
+                check_letters -= 1
+            if check_letters == 0:
 
-
-
-    check_letters = len(fruit) 
-
-    for letter in range(0,len(fruit)):
-         
-        if out[letter] != "_":
-            check_letters -= 1
-        if check_letters == 0:
-
+                game_state = False
+                print("Congratulation You won:- The Word was \"{fruit}\"") 
+        print(out)
+    else:
+        print(pics[chance])
+        chance +=1
+        if chance == 7:
             game_state = False
-            print("won") 
+            print(f"Game Over: The Word was \"{fruit}\"")
+            
 
-    print(out)
+            
