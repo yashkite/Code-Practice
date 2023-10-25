a5df74db96003ccd300b8a3015ebb81a09163f9f
sunghun <nokdonwithkj211@gmail.com>
2023-10-25 22:13:58 +0900

    test(AppTest): configure function for test codes

---
+++ Small Projects/Java/Calculator/calculator/src/main/java/com/example/calculator/testmethodset/CalculationFunction.java
@@ -0,0 +1,19 @@
+package com.example.calculator.testmethodset;
+
+public class CalculationFunction {
+    public static float divide(float num1, float num2) {
+        return num1 / num2;
+    }
+
+    public static float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    public static float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public static float add(float num1, float num2) {
+        return num1 + num2;
+    }
+}
--- Small Projects/Java/Calculator/calculator/src/test/java/com/example/calculator/AppTest.java
+++ Small Projects/Java/Calculator/calculator/src/test/java/com/example/calculator/AppTest.java
@@ -1,7 +1,8 @@
 package com.example.calculator;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import com.example.calculator.testmethodset.CalculationFunction;
 import org.junit.Test;
 
 /**
@@ -9,12 +10,94 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+    // Execute same as junit.Assert.assertEquals()
+    private boolean compareResult(float result, float expected) {
+        return result == expected;
+    }
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void addTwoPositiveNumber()
     {
-        assertTrue( true );
+        float addResult = CalculationFunction.add(1, 2);
+        assertEquals(addResult, 3,0.1);
+    }
+
+    @Test
+    public void addPositiveAndNegativeNumber()
+    {
+        float addResult = CalculationFunction.add(1, -2);
+        assertEquals(addResult, -1,0.1);
+    }
+
+    @Test
+    public void addTwoNegativeNumber()
+    {
+        float addResult = CalculationFunction.add(-1, -2);
+        assertEquals(addResult, -3,0.1);
+    }
+
+    @Test
+    public void subtractTwoPositiveNumber()
+    {
+        float subtractResult = CalculationFunction.subtract(1, 2);
+        assertEquals(subtractResult, -1,0.1);
+    }
+
+    @Test
+    public void subtractPositiveAndNegativeNumber()
+    {
+        float subtractResult = CalculationFunction.subtract(1, -2);
+        assertEquals(subtractResult, 3,0.1);
+    }
+
+    @Test
+    public void subtractTwoNegativeNumber()
+    {
+        float subtractResult = CalculationFunction.subtract(-1, -2);
+        assertEquals(subtractResult, 1,0.1);
+    }
+
+    @Test
+    public void multiplyTwoPositiveNumber()
+    {
+        float multiplyResult = CalculationFunction.multiply(1, 2);
+        assertEquals(multiplyResult, 2,0.1);
+    }
+
+    @Test
+    public void multiplyPositiveAndNegativeNumber()
+    {
+        float multiplyResult = CalculationFunction.multiply(1, -2);
+        assertEquals(multiplyResult, -2,0.1);
+    }
+
+    @Test
+    public void multiplyTwoNegativeNumber()
+    {
+        float multiplyResult = CalculationFunction.multiply(-1, -2);
+        assertEquals(multiplyResult, 2,0.1);
+    }
+
+    @Test
+    public void divideTwoPositiveNumber()
+    {
+        float divideResult = CalculationFunction.divide(1, 2);
+        assertEquals(divideResult, 0.5F,0.1);
+    }
+
+    @Test
+    public void dividePositiveAndNegativeNumber()
+    {
+        float divideResult = CalculationFunction.divide(1, -2);
+        assertEquals(divideResult, -0.5F,0.1);
+    }
+
+    @Test
+    public void divideTwoNegativeNumber()
+    {
+        float divideResult = CalculationFunction.divide(-1, -2);
+        assertEquals(divideResult, 0.5F,0.1);
     }
 }
