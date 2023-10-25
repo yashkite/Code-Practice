a2f533d61b85dfcd82afbcfc2e17e8ecb9165b2f
sunghun <nokdonwithkj211@gmail.com>
2023-10-26 08:43:30 +0900

    style(App): remove description

--- Small Projects/Java/Calculator/calculator/src/main/java/com/example/calculator/App.java
+++ Small Projects/Java/Calculator/calculator/src/main/java/com/example/calculator/App.java
@@ -12,7 +12,7 @@ public class App {
 
             System.out.println("-------**-------");
             System.out.println("1. Addition");
-            System.out.println("2. Subtraction"); // Changed to "Subtraction"
+            System.out.println("2. Subtraction");
             System.out.println("3. Multiplication");
             System.out.println("4. Division");
             System.out.println("5. Exit");
