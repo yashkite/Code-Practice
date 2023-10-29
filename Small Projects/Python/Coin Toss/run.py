d608ff7be0dd6031f9c47bb5c50ef6c48d0318cb
luvchurchill <46406654+luvchurchill@users.noreply.github.com>
2023-10-29 05:25:05 +0200

    Runs forever (until "q" input)

--- Small Projects/Python/Coin Toss/run.py
+++ Small Projects/Python/Coin Toss/run.py
@@ -1,7 +1,13 @@
 import random
 
-coin = random.randint(0,1)
-if coin == 0:
-    print("head")
-else:
-    print("tails")
+while True:
+    answer = input(" 'f' to flip the coin, 'q' to exit: \n ")
+    if answer == "f":
+        coin = random.randint(0, 1)
+        if coin == 1:
+            print("heads")
+        else:
+            print("tails")
+    elif answer == "q":
+        break
+
