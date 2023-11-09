23fe4fcb4f7d3875785f561fe1988e10b2d92e64
bakanabaka <shravanth28@gmail.com>
2023-11-10 00:05:48 +0530

    Added a while loop until they want to stop

--- Small Projects/Python/Calculator/run.py
+++ Small Projects/Python/Calculator/run.py
@@ -1,29 +1,32 @@
 def add(num1, num2):
     return num1 + num2
+
+
 def substract(num1, num2):
     return num1 - num2
+
+
 def multiply(num1, num2):
     return num1 * num2
+
+
 def divide(num1, num2):
     return num1 / num2
 
 
-operations  = {
-    "+" : add,
-    "-" : substract,
-    "*" : multiply,
-    "/" : divide
-}
+operations = {"+": add, "-": substract, "*": multiply, "/": divide}
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+while quit:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
 
-for symbol in operations:
-    print(symbol)
-operations_sysmbol = input("Enter the operation: ")
+    for symbol in operations:
+        print(symbol)
+    operations_sysmbol = input("Enter the operation: ")
 
-calculate = operations[operations_sysmbol]
+    calculate = operations[operations_sysmbol]
 
-answer = calculate(num1, num2)
+    answer = calculate(num1, num2)
 
-print(f"{num1} {operations_sysmbol} {num2} = {answer}")
+    print(f"{num1} {operations_sysmbol} {num2} = {answer}")
+    quit = int(input("Do you want to quit yes=0/no=1"))
