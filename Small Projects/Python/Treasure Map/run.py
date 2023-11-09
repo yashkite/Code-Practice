131aa85e09148027c3ccd086bed5698d07652ac2
Yash Kite <yashkite21@gmail.com>
2023-11-09 10:49:07 +0530

    documentation updated in treasure map project
    documentation updated in treasure map project

--- Small Projects/Python/Treasure Map/run.py
+++ Small Projects/Python/Treasure Map/run.py
@@ -1,16 +1,22 @@
+#default map style
 row1 = ["🥲","🥲","🥲"]
 row2 = ["🥲","🥲","🥲"]
 row3 = ["🥲","🥲","🥲"]
 
+#arrange the map vertically
 map = str(f"{row1}\n{row2}\n{row3}")
 
+#print default map
 print(map)
 
+#input to put the x there
 x_location = input("Enter the location in xy number format like \"23\" to put \"x\" there: ")
 
+#format text to row and column
 row = int(x_location[1])
 column = int(x_location[0])
 
+#check the input is valid and write the x on the position
 if 1<= row <=3  and 1<= column <=3 :
     match row:
         case 1:
@@ -23,6 +29,7 @@ if 1<= row <=3  and 1<= column <=3 :
 else:
     print("Invalid Input")
 
+#show the final map
 map= str(f"{row1}\n{row2}\n{row3}")
 print(map)
 
