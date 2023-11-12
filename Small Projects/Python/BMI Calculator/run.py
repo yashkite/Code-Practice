83fa40f3d4bd08d5fd03e79bb4b83cc6b763b54c
Yash Kite <yashkite21@gmail.com>
2023-11-13 01:28:56 +0530

    Show Output using PrettyTable Library (BMI Calculator)
    added the functionality to show output using PrettyTable and closes #28

--- Small Projects/Python/BMI Calculator/run.py
+++ Small Projects/Python/BMI Calculator/run.py
@@ -1,16 +1,33 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Height", "Weight", "BMI", "Category"]
+
+row = []
 height = float(input("Enter Your Hight in Meters: "))
+row.append(f"{height} M")
 weight = float(input("Enter Your Weight in Kg: "))
+row.append(f"{weight} Kg")
 
 bmi = weight/height ** 2
 bmi_text = "{:.2f}".format(bmi) 
-
+row.append(bmi_text)
 if bmi < 18.5 :
     print(f"Your Underweight and your BMI is {bmi_text}")
+    row.append("Underweight")
 elif  18.5 <= bmi <= 24.9:
     print(f"Your Normal and your BMI is {bmi_text}")
+    row.append("Normal")
 elif  25 <= bmi <= 29.9:
     print(f"Your Overweight and your BMI is {bmi_text}")
+    row.append("Overweight")
 elif  30 <= bmi <= 34.9:
     print(f"Your Obese and your BMI is {bmi_text}")
+    row.append("Obese")
 else:
     print(f"Your Extreamely Obese and your BMI is {bmi_text}")
+    row.append("Extremely Obese")
+
+table.add_row(row)
+
+print(table)
