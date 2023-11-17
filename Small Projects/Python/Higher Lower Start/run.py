c07331f91ad1ade283bac270b2fd6b8d966bb864
bakanabaka <shravanth28@gmail.com>
2023-11-17 18:08:20 +0530

    Added country and description for user

--- Small Projects/Python/Higher Lower Start/run.py
+++ Small Projects/Python/Higher Lower Start/run.py
@@ -1,51 +1,60 @@
+# run.py
+
 from database import data
 import random
 
-exit = False
+exit_game = False
 
-while not exit:
-    
-    points = 0 
+while not exit_game:
+    points = 0
     game_over = False
     option_b = random.choice(data)
+
     while not game_over:
         option_a = option_b
         option_b = random.choice(data)
+
         if option_a == option_b:
             continue
-        
+
         name_a = option_a["name"]
         name_b = option_b["name"]
         follow_count_a = option_a["follower_count"]
         follow_count_b = option_b["follower_count"]
-        print(f"1. {name_a}")
-        print(f"2. {name_b}")
-        
-        choice = int(input("Who has More followers 1 or 2: "))
+        description_a = option_a["description"]
+        country_a = option_a["country"]
+        description_b = option_b["description"]
+        country_b = option_b["country"]
+
+        print(f"1. {name_a} - {description_a} ({country_a})")
+        print(f"2. {name_b} - {description_b} ({country_b})")
+
+        choice = int(input("Who has more followers, 1 or 2? "))
 
         if choice == 1 and follow_count_a > follow_count_b:
-            print("Your Right")
+            print("You're right!")
             points += 1
             continue
         elif choice == 2 and follow_count_a < follow_count_b:
-            print("Your Right")
+            print("You're right!")
             points += 1
             continue
         else:
             print("Game Over")
-            print(f"You Won {points} points !!!")
+            print(f"You won {points} points!")
             game_over = True
 
-    play_again = int(input("""
+    play_again = int(
+        input(
+            """
 1. Play Again
 2. Exit
-Enter the Choice: """))
-    
+Enter the choice: """
+        )
+    )
+
     if play_again == 1:
         continue
     else:
-        exit = True
+        exit_game = True
         print("Exiting...")
-
-
-
